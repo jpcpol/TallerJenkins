1da4f391a578a644bc7dc2d4079be535ca1316a3
Juan Pablo Chancay <102939810+jpcpol@users.noreply.github.com>
2022-09-20 23:55:23 -0300

    JNPM Test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -35,10 +35,5 @@ describe('AppComponent', () => {
     const suma = 2+3;
     expect(suma).toBe(5);
   });
-
-  it('suma 3+3 = 6', () => {
-    const suma = 3+3;
-    expect(suma).toBe(6);
-  });
-
+  
 });
